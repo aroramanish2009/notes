611bd5ce8d200288e6ac4f38410b908875dc1e35
root <root@ip-172-26-3-150.us-west-2.compute.internal>
2023-11-26 07:53:26 +0000

    more stuff

--- pymisc/aoc-2022/dec7/d7script.py
+++ pymisc/aoc-2022/dec7/d7script.py
@@ -10,20 +10,23 @@ sizes = defaultdict(int)
 stack = []
 for c in commands:
     if c.startswith("$ ls") or c.startswith("dir"):
+        #Ignore these and move to next item in list
         continue
     if c.startswith("$ cd"):
         dest = c.split()[2]
         if dest == "..":
+            #Empty the stack if you see ..
             stack.pop()
         else:
+            #If stack has anything in it and you got cd xx then create subdict. If stack is empty then create path with new dest
             path = f"{stack[-1]}_{dest}" if stack else dest
             stack.append(path)
     else:
         size, file = c.split()
+        #for the path in stack .. add files size to sizes dict
         for path in stack:
             sizes[path] += int(size)
 
-print (sizes)
 needed_size = 30000000 - (70000000 - sizes["/"])
 for size in sorted(sizes.values()):
     if size > needed_size:
